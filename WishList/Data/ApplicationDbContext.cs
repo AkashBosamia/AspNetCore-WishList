d25deca36ffa0d7f9616732f9abf19fc7d92327d
Akash <akash19bosamia@gmail.com>
2020-10-14 19:28:04 +0530

    corrected property place

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -9,10 +9,12 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Item> Items { get; set; }
+       
         ApplicationDbContext(DbContextOptions options) : base(options)
         {
             
         }
+        
+        public DbSet<Item> Items { get; set; }
     }
 }
