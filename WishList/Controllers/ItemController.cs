1b7598b11421b8ac9ecdc4a4ff65d4e499401aba
Akash <akash19bosamia@gmail.com>
2020-10-14 22:28:36 +0530

    used first or default

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -39,7 +39,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int Id)
         {
-            var item = _context.Items.SingleOrDefault(x => x.Id == Id);
+            var item = _context.Items.FirstOrDefault(x => x.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
 
